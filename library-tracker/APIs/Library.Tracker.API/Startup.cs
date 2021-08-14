6be8001e54580685753a4ec5fbbe8af42227f78e
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-08-14 21:17:18 +0100

    fixed app secret, startup cleanup

--- library-tracker/APIs/Library.Tracker.API/Startup.cs
+++ library-tracker/APIs/Library.Tracker.API/Startup.cs
@@ -74,7 +74,7 @@ namespace Library.Tracker.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseRouting();
             // global cors policy
