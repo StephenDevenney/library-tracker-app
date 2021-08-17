57842ea4bca22e07f5a2546dc1cf653385d601cf
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-08-18 00:04:35 +0100

    book viewmodel and entity

--- library-tracker/Library.Tracker.Shared/ViewModels/BookViewModel.cs
+++ library-tracker/Library.Tracker.Shared/ViewModels/BookViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Library.Tracker.Shared.ViewModels
 {
     public class BookViewModel
     {
-        public string BookName { get; set; }
+        public Titles Title { get; set; }
+        public string[] Authors { get; set; }
         public string ISBN { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+        public ImageLinksObj ImageLinks { get; set; }
     }
+
+    public class Titles
+    {
+        public string BookName { get; set; }
+        public string BookSubTitle { get; set; }
+    }
+
+    public class ImageLinksObj 
+    { 
+        public string Small { get; set; }
+        public string Standard { get; set; }
+    }
+
 }
